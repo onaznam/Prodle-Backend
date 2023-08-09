f05ae14cd7673290443378375c6b7328d2f945b9
John-13-7 <galberto2573@googlemail.com>
2023-08-09 10:30:58 -0700

    oh please just let me be one of your memories

--- backend.js
+++ backend.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //So it can run locally
 app.use((req, res, next) => {
@@ -202,6 +202,6 @@ app.get("/" , (req,res) => {
   res.send("Hello to the hiroku");
 })
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
